5e26210727fa5dda0275db6bfda422803895a73b
manulei@gmail.com <manulei@gmail.com@a93a3935-6c21-4ee6-5bc1-4ef37895877b>
2012-05-16 16:20:23 +0000

    again, not a working copy yet, just to sync in.

--- hw3/src/fparser.c
+++ hw3/src/fparser.c
@@ -7,11 +7,29 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define N 100
 
 int main(){
-	char command[100];
-	fgets(command,100,stdin);
-	printf("%s",command);
-	system("pause");
+	char command[N];
+	do{
+		fgets(command,N,stdin);
+		if (strncmp(command,"BEGIN",strlen("BEGIN"))){//got begin
+			break;
+		}
+	} while (1);
+	do{
+		do{
+			fgets(command,N,stdin);
+			//list.insert(command);
+			//TODO: write a simple list to contain the commands.
+			printf("%s\n",command);
+		}while (strncmp(command,"BARRIER",strlen("BARRIER")));
+		//handle burst
+		printf("handling burst\n");
+		fgets(command,N,stdin);
+	} while (strncmp(command,"END",strlen("END")));//loop until end
+	//system("pause");
 	return 0;
 }
